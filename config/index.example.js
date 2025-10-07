2daecad21bfc7ae0e8c09f3bf17a025e791e040e
felpp25 <filiperamos1236@gmail.com>
2025-10-07 10:37:19 -0300

    Atualizado arquivo index.example.js

--- config/index.example.js
+++ config/index.example.js
@@ -1,7 +1,13 @@
-// config/config.example.js
+// config/index.example.js
+require('dotenv').config(); // 
+
 module.exports = {
-  chatwootUrl: "https://faculdadefgi.cloud.chatwoot.app.br",
-  chatwootApiKey: "<COLE_AQUI_SUA_CHATWOOT_API_KEY>",
-  chatwootAccountId: 1,
-  chatwootInboxId: 3,
+  chatwootUrl: process.env.CHATWOOT_URL || 'https://faculdadefgi.cloud.chatwoot.app.br',
+  chatwootApiKey: process.env.CHATWOOT_API_KEY || 'COLE_AQUI_SUA_CHAVE',
+  chatwootAccountId: process.env.CHATWOOT_ACCOUNT_ID ? parseInt(process.env.CHATWOOT_ACCOUNT_ID, 10) : 1, //ID da conta
+  chatwootInboxId: process.env.CHATWOOT_INBOX_ID ? parseInt(process.env.CHATWOOT_INBOX_ID, 10) : 3, // ID da inbox
 };
+
+// Ambos ID são localizados na URL do Chatwoot
+// Exemplo: https://faculdadefgi.cloud.chatwoot.app.br/accounts/1/inboxes/3/conversations
+// Aqui, 1 é o chatwootAccountId e 3 é o chatwootInboxId
